0e99971cea9ef182d4b44865d0b281f79c5ae277
touch <mikemoretti88@gmail.com>
2011-10-05 23:48:34 -0700

    Added password and encrypted_password attributes to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,29 @@
+class User < ActiveRecord::Base
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name, :presence => true,
+                            :length => { :maximum => 50 }
+
+  validates :email, :presence => true,
+                            :format => { :with => email_regex },
+                            :uniqueness => { :case_sensitive => false } # ":uniqueness => true" is implicit here
+
+  validates :password, :presence => true,
+                                  :confirmation => true,
+                                  :length => { :within => 6..40 }
+end
+
 # == Schema Information
 #
 # Table name: users
 #
-#  id               :integer(4)      not null, primary key
-#  name         :string(255)
-#  email         :string(255)
-#  created_at :datetime
-#  updated_at :datetime
+#  id                 :integer(4)      not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
 #
 
-class User < ActiveRecord::Base
-  attr_accessible :name, :email
-  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :name, :presence => true,
-                            :length => {:maximum => 50}
-
-  validates :email, :presence => true,
-                            :format => {:with => email_regex},
-                            :uniqueness => {:case_sensitive => false} # ":uniqueness => true" is implicit here
-end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,14 @@
-# == Schema Information
-#
-# Table name: users
-#
-#  id         :integer(4)      not null, primary key
-#  name       :string(255)
-#  email      :string(255)
-#  created_at :datetime
-#  updated_at :datetime
-#
-
 require 'spec_helper'
 
 describe User do
 
-  before(:each) do 
-    @attr = {:name => "Example User", :email => "user@example.com"}
+  before(:each) do
+    @attr = {
+        :name => "Example User",
+        :email => "user@example.com",
+        :password => "foobar",
+        :password_confirmation => "foobar"
+    }
   end
 
   it "should create a new instance given a valid attribute" do
@@ -26,47 +20,112 @@ describe User do
     no_name_user = User.new(@attr.merge(:name => ""))
     no_name_user.should_not be_valid
   end
-  
+
   it "should require an email address" do
     no_email_user = User.new(@attr.merge(:email => ""))
     no_email_user.should_not be_valid
   end
-  
+
   it "should reject names that are too long" do
     long_name = "a" * 51
     long_name_user = User.new(@attr.merge(:name => long_name))
     long_name_user.should_not be_valid
   end
-  
+
   it "should accetp valid email addresses" do
     email_addresses = %w[user@foo.com THE_USER@foo.org first.last@foo.jp]
     email_addresses.each do |email_address|
       valid_email_address = User.new(@attr.merge(:email => email_address))
       valid_email_address.should be_valid
-    end   
+    end
   end
-  
+
   it "should reject invalid email addresses" do
     email_addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
     email_addresses.each do |email_address|
       invalid_email_address = User.new(@attr.merge(:email => email_address))
       invalid_email_address.should_not be_valid
-    end   
+    end
   end
-  
+
   it "should reject duplicate email addresses" do
     User.create!(@attr)
     user_with_duplicate_email_addresses = User.new(@attr)
     user_with_duplicate_email_addresses.should_not be_valid
   end
-  
+
   it "should reject duplicate email addresses ignoring the case" do
     upcased_email_addresses = @attr[:email].upcase
     User.create!(@attr.merge(:email => upcased_email_addresses))
     user_with_duplicate_email_addresses = User.new(@attr)
     user_with_duplicate_email_addresses.should_not be_valid
   end
+
+  describe "passwords" do
   
+    before(:each) do
+      @user = User.new(@attr)
+    end
+    
+    it "should have a password attribute" do
+      @user.should respond_to(:password)
+    end
+    
+    it "should have a password confirmation attribute" do
+      @user.should respond_to(:password_confirmation)
+    end
+
+  end
+  
+  describe "password validation" do
+
+    it "should require a password" do
+      User.new(@attr.merge(:password => "", :password_confirmation => "")).
+          should_not be_valid
+    end
+    
+    it "should require a matching password confirmation" do
+      User.new(@attr.merge(:password_confirmation => "invalid")).
+          should_not be_valid
+    end
+    
+    it "should  reject short passwords" do
+      short_password = "a" * 5
+      hash = @attr.merge(
+          :password => short_password,
+          :password_confirmation => short_password)
+      User.new(hash).should_not be_valid
+    end
+    
+    it "should  reject long passwords" do
+      long_password = "a" * 41
+      hash = @attr.merge(
+          :password => long_password,
+          :password_confirmation => long_password)
+      User.new(hash).should_not be_valid
+    end
+  end
+
+  describe "password encription" do
+    before(:each) do
+      @user = User.create!(@attr)
+    end
+    
+    it "should have an encrypted password attribute" do
+      @user.should respond_to(:encrypted_password)
+    end
+  end
 end
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :integer(4)      not null, primary key
+#  name               :string(255)
+#  email              :string(255)
+#  created_at         :datetime
+#  updated_at         :datetime
+#  encrypted_password :string(255)
+#
 
